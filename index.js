74515e8981e1b09674d30b5cdc3e45aa4376634d
VinlaRose <109898811+VinlaRose@users.noreply.github.com>
2022-08-24 18:03:20 +0530

    updated

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
 const numberOfNotes = document.querySelectorAll(".number-of-notes");
+const msgShow = document.querySelectorAll("#show-message");
 
 const notes = [2000, 500, 100, 20, 10, 5, 1];
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
   hideMessage();
-  if (billAmount.value > 0) {
+  if(billAmount.value === "" || cashGiven.value === ""){
+    showMessage("Enter all fields.");
+  }else if(billAmount.value === cashGiven.value){
+    showMessage("No change required");
+  }
+  else if (billAmount.value > 0) {
     // console.log(billAmount.value)
     // console.log(cashGiven.value)
    
